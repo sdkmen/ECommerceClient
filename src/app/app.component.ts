cedcbe3e7e36f6d854ab6a96d646fdbfd44f0f1c
Seyfullah Dikmen <seyfullah_dikmen@hotmail.com>
2023-03-08 23:11:35 +0300

    Add client flies

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ECommerceClient';
+
+  constructor(){
+  }
+}
