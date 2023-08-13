26e2579e2832986310d2b308c0e3a0f575d676c9
Seyfullah Dikmen <sdkmenn@gmail.com>
2023-08-13 15:46:22 +0300

    Add guard mechanism

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,13 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     NgxSpinnerModule,
-    HttpClientModule
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => localStorage.getItem("accessToken"),
+        allowedDomains: ["localhost:7288"]
+      }
+    })
   ],
   providers: [{ provide: "baseUrl", useValue: "https://localhost:7288/api", multi: true }],
   bootstrap: [AppComponent]
