dd308fac70a7d657f2dbeed1eab702a91a12e9bb
Seyfullah Dikmen <sdkmenn@gmail.com>
2023-09-26 23:49:15 +0300

    Add http error handler with http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { UiModule } from './ui/ui.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
 import { SocialLoginModule, SocialAuthServiceConfig, GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 import {
@@ -15,6 +15,7 @@ import {
   FacebookLoginProvider
 } from '@abacritt/angularx-social-login';
 import { LoginComponent } from './ui/components/login/login.component';
+import { HttpErrorHandlerInterceptorService } from './services/common/http-error-handler-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -57,7 +58,8 @@ import { LoginComponent } from './ui/components/login/login.component';
         ],
         onError: err => console.log(err)
       } as SocialAuthServiceConfig
-    }
+    },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorHandlerInterceptorService, multi: true }
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -0,0 +1,50 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../ui/custom-toastr.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
+
+  constructor(private toastrService: CustomToastrService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(error => {
+      switch (error.status) {
+        case HttpStatusCode.Unauthorized:
+          this.toastrService.message("unauthorized error caught", "unauthorized", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.InternalServerError:
+          this.toastrService.message("internal server error caught", "InternalServerError", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.BadRequest:
+          this.toastrService.message("badrequest error caught", "BadRequest", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.NotFound:
+          this.toastrService.message("notfound error caught", "NotFound", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        default:
+          this.toastrService.message("unexpected error caught", "error", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+      }
+      return of(error);
+    }));
+  }
+}
