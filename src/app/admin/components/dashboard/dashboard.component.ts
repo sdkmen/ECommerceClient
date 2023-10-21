da9b9512f344ccd90628858a469b4d2acc15e64a
Seyfullah Dikmen <sdkmenn@gmail.com>
2023-10-21 16:12:22 +0300

    Connect client with signalR

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HubUrls } from 'src/app/constants/hub-urls';
+import { ReceiveFunctions } from 'src/app/constants/receive-functions';
+import {
+  AlertifyService,
+  MessageType,
+  Position,
+} from 'src/app/services/admin/alertify.service';
+import { SignalRService } from 'src/app/services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
-
-  constructor(spinner: NgxSpinnerService) {
+  constructor(
+    spinner: NgxSpinnerService,
+    private signalRService: SignalRService,
+    private alertify: AlertifyService
+  ) {
     super(spinner);
+    signalRService.start(HubUrls.ProductHub);
   }
 
   ngOnInit(): void {
+    this.signalRService.on(
+      ReceiveFunctions.ProductAddedMessageReceiveFunction,
+      (message) => {
+        this.alertify.message(message, {
+          messageType: MessageType.Notify,
+          position: Position.TopCenter,
+        });
+      }
+    );
   }
 }
